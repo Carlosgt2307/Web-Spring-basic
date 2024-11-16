07e7bc9e2c673004149fc1830a99f8b5f2dff659
Carlosgt2307 <carlosagonzalestorres@gmail.com>
2024-11-16 14:10:45 -0500

    Feat: Vista Clima

--- src/main/java/com/example/tarea/HolaMundoController.java
+++ src/main/java/com/example/tarea/HolaMundoController.java
@@ -2,6 +2,12 @@ package com.example.tarea;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HolaMundoController {
@@ -9,5 +15,18 @@ public class HolaMundoController {
     @GetMapping("/hola")
     public String holaMundo() {
         return "hola";
+    }
+
+    @GetMapping("/clima")
+    public String mostrarClima() {
+        return "clima";
+    }
+
+     @GetMapping("/data/clima")
+    @ResponseBody
+    public List<Map<String, Object>> obtenerClima() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File("src/main/resources/static/data/clima.json");
+        return mapper.readValue(file, List.class);
     }
 }
